86056e666ca6711480475a573f45b6a5b07d7071
Francois De Lattre <miggu69@gmail.com>
2025-11-20 17:15:25 +0100

    Initial Commit

---
+++ SecureChat/SecureChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureChatApp.swift
+//  SecureChat
+//
+//  Created by Francois De Lattre on 20.11.25.
+//
+
+import SwiftUI
+
+@main
+struct SecureChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
